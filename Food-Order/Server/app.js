aa42e4c7eb6a9d77fe7b890e5ee2f3ec4db36cea
Prajukta10 <prajuktap10@gmail.com>
2025-06-09 21:59:21 +0530

    Update app.js

--- Food-Order/Server/app.js
+++ Food-Order/Server/app.js
@@ -4,41 +4,106 @@ const cors = require("cors");
 const route = require("./Route/index");
 const dotenv = require("dotenv");
 const passport = require("passport");
-const cookieSession = require("cookie-session");
-
-const PORT = process.env.PORT || 5500;
-const HOSTNAME = "0.0.0.0";
-const paymentRoute = require("./Controller/payment")
+const session = require("express-session");
+const paymentRoute = require("./Controller/payment");
 const authRoute = require("./Controller/auth");
 const passportSetup = require("./Controller/passport");
 
-const corsOptions = {
-    origin: "https://food-ordering-zone.netlify.app",
-    credentials: true,
-    optionSuccessStatus: 200
-}
-
 dotenv.config();
 
-// Request Management
 const app = express();
 
-app.use(cookieSession({name: "session", keys:["edureka"], maxAge: 24*60*60*1000}))
+const corsOptions = {
+  origin: "https://food-ordering-zone.netlify.app", 
+  credentials: true, 
+  optionSuccessStatus: 200,
+};
 
-app.use(express.json());      
+app.use(cors(corsOptions));
+
+
+app.use(express.json());
+
+app.use(
+  session({
+    secret: "K39*Rp2L@9zWq#tD0m",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: "none", 
+      secure: true,   
+    },
+  })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(cors(corsOptions));
-app.use('/', route);
-app.use('/api/payment/', paymentRoute);
+
+app.use("/", route);
+app.use("/api/payment/", paymentRoute);
 app.use("/auth/", authRoute);
-// DB
+
 const MongoAtlas = process.env.MONGODB_URI;
-mongoose.connect(MongoAtlas, {
-})
-    .then(res => {
-        app.listen(PORT, HOSTNAME, () => {
-            console.log(`Server is running at ${HOSTNAME}: ${PORT}`)
-        });
-    })
-    .catch(err => console.log(err));
+const PORT = 5500;
+
+mongoose
+  .connect(MongoAtlas)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+// const route = require("./Route/index");
+// const dotenv = require("dotenv");
+// const passport = require("passport");
+// const cookieSession = require("cookie-session");
+
+// const PORT = process.env.PORT || 5500;
+// const HOSTNAME = "0.0.0.0";
+// const paymentRoute = require("./Controller/payment")
+// const authRoute = require("./Controller/auth");
+// const passportSetup = require("./Controller/passport");
+
+// const corsOptions = {
+//     origin: "https://food-ordering-zone.netlify.app",
+//     credentials: true,
+//     optionSuccessStatus: 200
+// }
+
+// dotenv.config();
+
+// // Request Management
+// const app = express();
+
+// app.use(cookieSession({name: "session", keys:["edureka"], maxAge: 24*60*60*1000}))
+
+// app.use(express.json());      
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(cors(corsOptions));
+// app.use('/', route);
+// app.use('/api/payment/', paymentRoute);
+// app.use("/auth/", authRoute);
+// // DB
+// const MongoAtlas = process.env.MONGODB_URI;
+// mongoose.connect(MongoAtlas, {
+// })
+//     .then(res => {
+//         app.listen(PORT, HOSTNAME, () => {
+//             console.log(`Server is running at ${HOSTNAME}: ${PORT}`)
+//         });
+//     })
+//     .catch(err => console.log(err));
